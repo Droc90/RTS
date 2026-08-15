f4672a9da580cfbe28821c012fe061009827b397
Dan O <droconnell2000@gmail.com>
2026-08-15 16:44:41 -0400

    establish RTS application foundation and add Radzen UI

--- RTS.Web/Program.cs
+++ RTS.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
+using Radzen;
 using RTS.Application.Identity;
 using RTS.Infrastructure;
 using RTS.Infrastructure.Identity;
@@ -115,6 +116,7 @@ builder.Services.AddHsts(options =>
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddRadzenComponents();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
